85a5e0b15df7cfd9051dd5ac92858990ca8cfb97
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-08-06 18:24:12 +0200

    3.1.2 - UserRepository tests

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -4,11 +4,11 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,11 +30,13 @@ public class UserRepositoryTest {
     private UserRepository repository;
 
     private User user;
+    final String NOT_IMPORTANT = "xD";
 
     @Before
     public void setUp() {
         user = new User();
         user.setFirstName("Jan");
+        user.setLastName("Kowalski");
         user.setEmail("john@domain.com");
         user.setAccountStatus(AccountStatus.NEW);
     }
@@ -46,7 +48,7 @@ public class UserRepositoryTest {
 
         assertThat(users, hasSize(0));
     }
-    
+
     @Test
     public void shouldFindOneUsersIfRepositoryContainsOneUserEntity() {
         User persistedUser = entityManager.persist(user);
@@ -66,4 +68,71 @@ public class UserRepositoryTest {
         assertThat(persistedUser.getId(), notNullValue());
     }
 
+
+    @Test
+    public void findUserByFirstName() {
+        repository.save(user);
+        List<User> usersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+                user.getFirstName(), NOT_IMPORTANT, NOT_IMPORTANT);
+        assertThat(usersList, hasSize(1));
+    }
+
+    @Test
+    public void findUserByLastName() {
+        repository.save(user);
+        List<User> usersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+                NOT_IMPORTANT, user.getLastName(), NOT_IMPORTANT);
+        assertThat(usersList, hasSize(1));
+    }
+
+    @Test
+    public void findUserByEmail() {
+        repository.save(user);
+        List<User> usersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+                NOT_IMPORTANT, NOT_IMPORTANT, user.getEmail());
+        assertThat(usersList, hasSize(1));
+    }
+
+    @Test
+    public void findUserByFirstNameUpperCase() {
+        repository.save(user);
+        List<User> usersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+                user.getFirstName().toUpperCase(), NOT_IMPORTANT, NOT_IMPORTANT);
+        assertThat(usersList, hasSize(1));
+    }
+
+    @Test
+    public void findUserByFirstNameLowerCase() {
+        repository.save(user);
+        List<User> usersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+                user.getFirstName().toLowerCase(), NOT_IMPORTANT, NOT_IMPORTANT);
+        assertThat(usersList, hasSize(1));
+    }
+
+    @Test
+    public void findTwoUsersWithTheSameFirstName() {
+        User similarUser;
+        similarUser = new User();
+        similarUser.setFirstName(user.getFirstName());
+        similarUser.setLastName("Nowak");
+        similarUser.setEmail("destroyer@interia.com");
+        similarUser.setAccountStatus(AccountStatus.NEW);
+
+        repository.save(user);
+        repository.save(similarUser);
+
+        repository.save(user);
+        List<User> usersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+                user.getFirstName(), NOT_IMPORTANT, NOT_IMPORTANT);
+        assertThat(usersList, hasSize(2));
+    }
+
+    @Test
+    public void shouldNotFindAnyUser() {
+        repository.save(user);
+        String strangeName = user.getFirstName() + "impossibletohavethatname";
+        List<User> usersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+                strangeName, NOT_IMPORTANT, NOT_IMPORTANT);
+        assertThat(usersList, hasSize(0));
+    }
 }
